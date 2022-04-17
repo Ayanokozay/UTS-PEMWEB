44b644ac631e2025f1da19d19910f09b2a4dd1ee
M. Zainul Ulum <zayvseverybody@gmail.com>
2022-04-17 14:22:53 +0700

    uts

--- soal5/index.php
+++ soal5/index.php
@@ -1,62 +1,64 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>Menampilkan Excel</title>
+    <title>Soal 5</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
+
 <body>
-	
-	<style type="text/css">
-		body{
-			font-family: sans-serif;
-			text-align: center;
-		}
-		p{
-			color: green;
-		}
-	</style>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow-sm fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">M. Zainul Ulum</a>
+        </div>
+    </nav>
+    <div class="container mt-5">
+        <br />
+        <h3 align="center">Menampilkan Data File Excel</h3>
+        <br />
+        <div class="table-responsive">
+            <span id="message"></span>
+            <form method="post" id="load_excel_data" enctype="multipart/form-data">
+                <table class="table">
+                    <tr>
+                        <td width="25%" align="right">Pilih File</td>
+                        <td width="50%"><input type="file" id="select_excel" name="select_excel" /></td>
+                        <td width="25%"><input type="submit" id="load" name="load" class="btn btn-primary" /></td>
+                    </tr>
+                </table>
+            </form>
+            <br />
+            <table class="table" id="show_excel_data">
+                <!-- // Loading data  -->
 
-	<h2>M. Zainul Ulum <br>20050974028</h2>
+            </table>
+        </div>
+    </div>
 
-	<a href="upload.php">IMPORT FILE EXCEL</a>
-
-	<?php
-		if(isset($_GET['berhasil'])){
-			echo "<p>".$_GET['berhasil']." Data berhasil di import.</p>";
-		}
-
-	?>
-
-	<br><br>
-	<table border="1" align="center" width="500">
-		<tr>
-			<th>No</th>
-			<th>Nim</th>
-			<th>Nama</th>
-			<th>Jk</th>
-			<th>Prodi</th>
-			<th>Alamat</th>
-			<th>Kota</th>
-			<th>Email</th>
-		</tr>
-		<?php
-			include "koneksi.php";
-			$no = 1;
-			$tampil = mysqli_query($koneksi, "SELECT * from tmahasiswa");
-			while($data = mysqli_fetch_array($tampil)) :
-
-		?>
-			<tr>
-				<td><?=$no++?></td>
-				<td><?=$data['Nim']?></td>
-				<td><?=$data['Nama']?></td>
-				<td><?=$data['Jk']?></td>
-				<td><?=$data['Prodi']?></td>
-				<td><?=$data['Alamat']?></td>
-				<td><?=$data['Kota']?></td>
-				<td><?=$data['Email']?></td>
-			</tr>
-		<?php endwhile; ?>
-	</table>
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 </body>
+
+
 </html>
+
+<script>
+$(document).ready(function() {
+    $('#load_excel_data').on('submit', function(event) {
+        event.preventDefault();
+        $.ajax({
+            url: "fetch_rquest.php",
+            method: "POST",
+            data: new FormData(this), // It automatically capture form data 
+            contentType: false, // contentType When sending data to the server, use this content type.
+            cache: false,
+            processData: false,
+            success: function(data) {
+                $('#show_excel_data').html(data);
+                $('table').css('width', '100%');
+            }
+        })
+    });
+});
+</script>
