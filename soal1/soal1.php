8013f44c682a73867a19c97174905d7565561919
M. Zainul Ulum <zayvseverybody@gmail.com>
2022-04-17 09:08:10 +0700

    uts

---
+++ soal1/soal1.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "<h1>TABEL DATA BUKU</h1>";
+
+echo"<table width=500 border=1>
+
+<tr><td><b><center>NO</td><td><b><center>Judul Buku</td><td><b><center>Pengarang</td><td><b><center>Tahun</td></tr>
+<tr><td><b><center>1</td><td>Pemrograman Web dengan Laravel</td><td>Budi Sutejo</td><td>2019</td></tr>
+<tr><td><b><center>2</td><td>Belajar Mandiri Python</td><td>Ahmad Arifin</td><td>2010</td></tr>
+<tr><td><b><center>3</td><td>Microsoft Word untuk Penulisan Ilmiah</td><td>Edi Rahmadi</td><td>2015</td></tr>
+
+
+</table>";
+
+?>
---
+++ soal2/soal2.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "<h1>TABEL PROVINSI</h1>";
+
+echo"<table width=500 border=1>
+
+<tr><td><b><center>NO</td><td><b><center>Provinsi</td><td><b><center>Kabupaten</td><td><b><center>Kecamatan</td></tr>
+<tr><td><b><center>1</td><td>Jawa Timur</td><td>Surabaya</td><td>Jambangan</td></tr>
+<tr><td><b><center>2</td><td>Jawa Timur</td><td>Surabaya</td><td>Wonokromo</td></tr>
+<tr><td><b><center>3</td><td>Jawa Timur</td><td>Sidoarjo</td><td>Taman</td></tr>
+<tr><td><b><center>4</td><td>Jawa Timur</td><td>Sidoarjo</td><td>Waru</td></tr>
+<tr><td><b><center>5</td><td>Jawa Tengah</td><td>Semarang</td><td>Kecamatan 1</td></tr>
+<tr><td><b><center>6</td><td>Jawa Tengah</td><td>Semarang</td><td>Kecamatan 2</td></tr>
+<tr><td><b><center>7</td><td>Jawa Tengah</td><td>Solo</td><td>Kecamatan 1</td></tr>
+<tr><td><b><center>8</td><td>Jawa Tengah</td><td>Solo</td><td>Kecamatan 1</td></tr>
+
+</table>";
+
+?>
